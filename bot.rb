f33c5023b595729ef3c0bf5679eeb92d24c96c12
sylvain.abelard+git@gmail.com <sylvain.abelard+github@gmail.com>
2013-09-04 17:19:39 +0200

    Guybrush now speaks french

--- bot.rb
+++ bot.rb
@@ -65,6 +65,72 @@ bot = TweetBot.configure do |config|
     responses << "I'm glad to hear you attended your family reunion."
   end
 
+  # FRENCH
+
+  config.respond_to_phrase "Tu te bats comme un boeuf" do |responses|
+    responses << "C'est parfait, car toi tu te bats comme une vache."
+  end
+
+  config.respond_to_phrase "Sous peu je vais t'embrocher avec mon épée" do |responses|
+    responses << "D'abord, arrête de la brandir comme un plumeau !"
+  end
+
+  config.respond_to_phrase "Bave pas comme ça, ton épée va déraper" do |responses|
+    responses << "Je vais te la fermer une fois pour toutes."
+  end
+
+  config.respond_to_phrase "Ca se voit que l'alcool t'a imbibé le cerveau" do |responses|
+    responses << "Je bois pour pouvoir tolérer les gens comme toi."
+  end
+
+  config.respond_to_phrase "Je vais t'aider à te raser" do |responses|
+    responses << "Laisse tomber j'ai déjà fait ma toilette !"
+  end
+
+  config.respond_to_phrase "J'avais un chien qui était plus intelligent que toi" do |responses|
+    responses << "Il a dû t'apprendre tout ce que tu sais."
+  end
+
+  config.respond_to_phrase "Tu peux encore prendre tes jambes à ton cou." do |responses|
+    responses << "Rien à faire, je te tiendrai tête jusqu'au bout !"
+  end
+
+  config.respond_to_phrase "Tu ne peux pas comparer ton esprit au mien, pauvre idiot" do |responses|
+    responses << "Si tu l'utilises, je vais vraiment avoir peur."
+  end
+
+  config.respond_to_phrase "Ta voix n'a pas encore mué" do |responses|
+    responses << "Excuse-moi mais j'ai fait un tube l'été dernier."
+  end
+
+  config.respond_to_phrase "J'ai parlé à des singes plus polis que toi" do |responses|
+    responses << "Je vois que tu as passé du temps chez tes parents."
+  end
+
+  config.respond_to_phrase "Les gens tombent comme des mouches quand j'arrive" do |responses|
+    responses << "Avant MÊME de sentir ton haleine ?"
+  end
+
+  config.respond_to_phrase "Je ne trouve pas de mot pour exprimer mon dégoût" do |responses|
+    responses << "Je peux t'aider à trouver les mots si tu veux."
+  end
+
+  config.respond_to_phrase "Tu peux pas te payer des pantalons plus longs" do |responses|
+    responses << "J'ai gagné le concours des beaux mollets musclés."
+  end
+
+  config.respond_to_phrase "Tu te comportes comme un mendiant" do |responses|
+    responses << "J'essayais de me mettre à ton niveau."
+  end
+
+  config.respond_to_phrase "Tu portes encore des couches" do |responses|
+    responses << "Pour être marin, il faut savoir se mouiller."
+  end
+
+  config.respond_to_phrase "Alors, tu baffouilles quand tu parles aux femmes" do |responses|
+    responses << "Les femmes se battent pour s'approcher de moi."
+  end
+
   config.twitter_auth = TwitterAuth::AuthKeys
 end
 
